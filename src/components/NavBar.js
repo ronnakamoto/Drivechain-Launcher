edfa964103d0e3297e56235056a3e589d4a2d087
Ron Nakamoto <14256602+ronnakamoto@users.noreply.github.com>
2025-05-13 04:17:29 +0530

    feat: updated navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,29 +3,44 @@ import { NavLink } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { showSettingsModal } from '../store/settingsModalSlice';
 import styles from './NavBar.module.css';
+import { ReactComponent as ChainIcon } from '../assets/icons/chain-icon.svg';
+import { ReactComponent as WalletIcon } from '../assets/icons/wallet-icon.svg';
+import { ReactComponent as WithdrawalIcon } from '../assets/icons/withdrawal-icon.svg';
+import { ReactComponent as SettingsIcon } from '../assets/icons/settings-icon.svg';
 
 const NavBar = () => {
   const dispatch = useDispatch();
 
   return (
     <nav className={styles.nav}>
-      <div className={styles.leftSection}>
-        <NavLink to="/chains" className={styles.navLink}>
-          Chains
+      <div className={styles.logo}>
+        <span className={styles.logoText}>Drivechain</span>
+      </div>
+      
+      <div className={styles.navLinks}>
+        <NavLink to="/chains" className={({isActive}) => `${styles.navLink} ${isActive ? styles.active : ''}`}>
+          <ChainIcon className={styles.icon} />
+          <span>Chains</span>
         </NavLink>
-        <NavLink to="/wallet" className={styles.navLink}>
-          Wallet
+        <NavLink to="/wallet" className={({isActive}) => `${styles.navLink} ${isActive ? styles.active : ''}`}>
+          <WalletIcon className={styles.icon} />
+          <span>Wallet</span>
         </NavLink>
-        <NavLink to="/fast-withdrawal" className={styles.navLink}>
-          Fast Withdrawal
+        <NavLink to="/fast-withdrawal" className={({isActive}) => `${styles.navLink} ${isActive ? styles.active : ''}`}>
+          <WithdrawalIcon className={styles.icon} />
+          <span>Fast Withdrawal</span>
         </NavLink>
       </div>
-      <div className={styles.iconContainer}>
+      
+      <div className={styles.rightSection}>
         <button 
           className={styles.settingsButton}
           onClick={() => dispatch(showSettingsModal())}
+          aria-label="Settings"
+          title="Open Settings"
         >
-          Settings
+          <SettingsIcon className={styles.settingsIcon} />
+          <span className={styles.settingsLabel}>Settings</span>
         </button>
       </div>
     </nav>
